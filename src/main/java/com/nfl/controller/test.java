ca14e7458e7ea2ba2d75da5b2067e92044554e69
chenjian <779187545@qq.com>
2016-06-14 19:23:05 +0800

    .

--- src/main/java/com/nfl/controller/test.java
+++ src/main/java/com/nfl/controller/test.java
@@ -1,16 +1,10 @@
 package com.nfl.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 import com.nfl.pojo.NflUsers;
 import com.nfl.serviceImp.UserServiceImp;
