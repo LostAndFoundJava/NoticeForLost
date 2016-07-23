c6726c3c341a9c6186aaf39707cb28a2a0fb536e
chencheng <chencheng@chenjiandeMacBook-Pro.local>
2016-07-23 15:35:25 +0800

    delete the tag table

--- src/main/resources/nfl.sql
+++ src/main/resources/nfl.sql
@@ -37,17 +37,18 @@ CREATE TABLE `nfl_posts` (
   `comment_count` int(11) NOT NULL DEFAULT '0',
   `like_count` int(11) NOT NULL DEFAULT '0',
   `share_count` int(11) NOT NULL DEFAULT '0',
+  `topic_id` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `nfl_post_tag`;
 
-CREATE TABLE `nfl_post_tag` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_id` int(11) NOT NULL DEFAULT 0,
-  `topic_id` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+--CREATE TABLE `nfl_post_tag` (
+--  `id` int(11) NOT NULL AUTO_INCREMENT,
+--  `post_id` int(11) NOT NULL DEFAULT 0,
+--  `topic_id` int(11) NOT NULL DEFAULT 0,
+--  PRIMARY KEY (`id`)
+--) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `nfl_topics`;
 
