cb52038b9be2602448d788c0d6d3e5183c61019e
p0pfan <chch1227@gmail.com>
2016-07-23 15:46:37 +0800

    ..

--- src/main/resources/nfl.sql
+++ src/main/resources/nfl.sql
@@ -18,76 +18,76 @@ USE `nfl`;
 DROP TABLE IF EXISTS `nfl_photos`;
 
 CREATE TABLE `nfl_photos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(45) NOT NULL,
-  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `post_id` int(11) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `key` VARCHAR(45) NOT NULL,
+  `ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `nfl_posts`;
 
 CREATE TABLE `nfl_posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `post_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `post_content` longtext,
-  `post_status` int(2)NOT NULL DEFAULT '0',
-  `post_type` int(1) NOT NULL,
-  `comment_count` int(11) NOT NULL DEFAULT '0',
-  `like_count` int(11) NOT NULL DEFAULT '0',
-  `share_count` int(11) NOT NULL DEFAULT '0',
-  `topic_id` int(11) NOT NULL DEFAULT '1',
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `post_ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_content` LONGTEXT,
+  `post_status` INT(2)NOT NULL DEFAULT '0',
+  `post_type` INT(1) NOT NULL,
+  `comment_count` INT(11) NOT NULL DEFAULT '0',
+  `like_count` INT(11) NOT NULL DEFAULT '0',
+  `share_count` INT(11) NOT NULL DEFAULT '0',
+  `topic_id` INT(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `nfl_post_tag`;
 
---CREATE TABLE `nfl_post_tag` (
+-- CREATE TABLE `nfl_post_tag` (
 --  `id` int(11) NOT NULL AUTO_INCREMENT,
 --  `post_id` int(11) NOT NULL DEFAULT 0,
 --  `topic_id` int(11) NOT NULL DEFAULT 0,
 --  PRIMARY KEY (`id`)
---) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+-- ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `nfl_topics`;
 
 CREATE TABLE `nfl_topics` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `topic_name` varchar(30) NOT NULL,
-  `add_ts` datetime,
-  `count` int(30) NOT NULL DEFAULT 0,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `topic_name` VARCHAR(30) NOT NULL,
+  `add_ts` DATETIME,
+  `count` INT(30) NOT NULL DEFAULT 0,
    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `nfl_users`;
 
 CREATE TABLE `nfl_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(50) NOT NULL,
-  `user_email` varchar(100),
-  `user_phone` varchar(100),
-  `user_pwd` varchar(100) NOT NULL,
-  `user_registered_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_status` int(11) NOT NULL,
-  `user_activationKey` varchar(24),
-  `user_avatar` varchar(100),
-  `user_desc` varchar(200),
-  `resetpwd_key` varchar(100),
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(50) NOT NULL,
+  `user_email` VARCHAR(100),
+  `user_phone` VARCHAR(100),
+  `user_pwd` VARCHAR(100) NOT NULL,
+  `user_registered_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_status` INT(11) NOT NULL,
+  `user_activationKey` VARCHAR(24),
+  `user_avatar` VARCHAR(100),
+  `user_desc` VARCHAR(200),
+  `resetpwd_key` VARCHAR(100),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `nfl_post_relation`;
 
 CREATE TABLE `nfl_post_relation` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `commented_msg_id` int(11) NOT NULL,
-  `commented_user_id` int(11) NOT NULL,
-  `comment_msg_id` int(11) NOT NULL,
-  `comment_user_id` int(11) NOT NULL,
-  `comment_type` int(1) NOT NULL,
-  `comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `page_index` int(3),
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `commented_msg_id` INT(11) NOT NULL,
+  `commented_user_id` INT(11) NOT NULL,
+  `comment_msg_id` INT(11) NOT NULL,
+  `comment_user_id` INT(11) NOT NULL,
+  `comment_type` INT(1) NOT NULL,
+  `comment_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `page_index` INT(3),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
